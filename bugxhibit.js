d1bc06d6ab81294ee139ece59b3fd4ec60626f8f
Andrew Sutherland <asutherland@asutherland.org>
2009-05-27 23:31:42 -0700

    initial hooray!

---
+++ bugxhibit.js
@@ -0,0 +1,85 @@
+
+// this comes from using ctype=csv
+var columns = ["priority","changeddate","keywords","assigned_to","qa_contact",
+               "bug_severity","opendate","resolution","classification",
+               "qa_contact_realname","alias","op_sys","cf_blocking_fennec",
+               "assigned_to_realname","reporter","rep_platform",
+               "short_short_desc","status_whiteboard","votes","bug_status",
+               "version","component","reporter_realname","product",
+               "target_milestone","patches"];
+
+/**
+ * Split a string, returning only non-empty values.
+ *
+ * Examples using /[[\]]+/:
+ * "" => []
+ * "[foo][bar]" => ["foo", "bar"]
+ * "[foo bar]" => ["foo bar"]
+ */
+function splitIgnoreEmpty(bstr, pat) {
+  if (!bstr)
+    return [];
+
+  var bits = bstr.split(pat);
+  var result = [];
+  // filter out empty entries
+  for (var i = 0; i < bits.length; i++) {
+    if (bits[i])
+      result.push(bits[i]);
+  }
+  return result;
+}
+
+function bugzillaConverter() {
+  var data = {items: [],
+              properties: {
+                patchCount: {valueType: "number"},
+                votes: {valueType: "number"},
+                opendate: {valueType: "date"},
+                changeddate: {valueType: "date"}
+                }
+              };
+  var items = data.items;
+  var rawBug, goodBug, iCol;
+
+  for (var key in bugs) {
+    if (parseInt(key) == NaN)
+      continue;
+    rawBug = bugs[key];
+    goodBug = {num: key};
+    items.push(goodBug);
+
+    for (iCol = 0; iCol < rawBug.length; iCol++) {
+      goodBug[columns[iCol]] = rawBug[iCol];
+    }
+
+    goodBug.label = key + ": " + goodBug.short_short_desc;
+    goodBug.whiteboard_bits =
+      splitIgnoreEmpty(goodBug.status_whiteboard, /[[\]]+/);
+    goodBug.keyword_bits = splitIgnoreEmpty(goodBug.keywords);
+    goodBug.patchCount = splitIgnoreEmpty(goodBug.patches, ",").length;
+    goodBug.resolution = goodBug.resolution || "--";
+  }
+
+  return data;
+}
+
+function buglistCallback() {
+  var nextCallbackNum = Exhibit.JSONPImporter._callbacks.next || 2;
+  var thisCallbackNum = nextCallbackNum - 1;
+  Exhibit.JSONPImporter._callbacks["cb" + thisCallbackNum]();
+}
+
+/**
+ *
+ */
+function exhibitLoaded() {
+  console.log("exhibit loaded");
+  $(".exhibit-facet-header").click(
+    function() {
+      $(this).toggleClass("collapsed").next().toggle("fast");
+      return false;
+    }).toggleClass("collapsed").next().hide();
+  $(".exhibit-facet[defaultExpanded=true]").children()
+    .removeClass("collapsed").next().show();
+}
